f4a296c3f0ead89e8821561b9544e7849d411db2
Mike McBride <mmcbride1007@gmail.com>
2020-03-27 11:50:24 -0500

    swap inline and flex screens

--- src/components/StatCard.svelte
+++ src/components/StatCard.svelte
@@ -6,7 +6,7 @@ $: deaths = Intl.NumberFormat().format(_get(stats, 'deaths.value', 0))
 $: recovered = Intl.NumberFormat().format(_get(stats, 'recovered.value', 0))
 </script>
 
-<article class="border border-gray-300 rounded-sm p-4 inline-flex flex-col flex-1 justify-between lg:py-8 lg:px-0 lg:flex lg:flex-row">
+<article class="border border-gray-300 rounded-sm p-4 flex flex-col flex-1 justify-between lg:py-8 lg:px-0 lg:inline-flex lg:flex-row">
   <div class="flex flex-col-reverse justify-center p-0 mb-4 lg:mb-0 lg:flex-col lg:text-center lg:py-0 lg:px-8">
     <span class="text-5xl font-normal mb-2">{confirmed}</span>
     <span class="uppercase font-normal text-sm tracking-wide">confirmed</span>
